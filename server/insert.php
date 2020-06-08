d9cb26b08fbc6b26dd245cd9b6044557e0ab8c35
lijinho <58988274+lijinho@users.noreply.github.com>
2020-06-08 05:02:00 -0300

    add insert.php

--- server/insert.php
+++ server/insert.php
@@ -4,3 +4,12 @@
 
   $templage_name = $_POST['template_name'];
   $contain = $_POST['contain'];
+
+  $sql = "INSERT INTO template (template_name, contain) VALUES('$templage_name','$contain')";
+
+  if ($conn->query($sql) === true){
+    echo "New template was recorded successfully";
+  }
+  else {
+    echo "error";
+  }
